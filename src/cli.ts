3dd2c010624eafd472bd3cd0298bfc6610037def
Dmitry Patsura <talk@dmtry.me>
2018-11-07 20:02:21 +0900

    Feature: Run TSC diagnostics

--- src/cli.ts
+++ src/cli.ts
@@ -11,10 +11,25 @@ const options = {
 const files = [
     'sandbox/do-simple-math.ts'
 ];
+
 const host = ts.createCompilerHost(options);
 const program = ts.createProgram(files, options, host);
+const diagnostics = ts.getPreEmitDiagnostics(program);
 
-// console.log(ast);
+if (diagnostics.length) {
+    diagnostics.forEach(diagnostic => {
+        const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+        if (!diagnostic.file) {
+            console.log(message);
+            return;
+        }
+
+        const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+        console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+    });
+
+    process.exit(1);
+}
 
 llvm.initializeAllTargetInfos();
 llvm.initializeAllTargets();
