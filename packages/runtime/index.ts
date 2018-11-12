5f91c40080434298b8a250f6aed5cd3bc6d6fb10
Dmitry Patsura <talk@dmtry.me>
2018-11-12 23:25:01 +0300

    Feature(runtime): Introduce RUNTIME_DEFINITION_FILE

--- packages/runtime/index.ts
+++ packages/runtime/index.ts
@@ -2,3 +2,5 @@
 import * as path from "path";
 
 export const RUNTIME_ARCHIVE_FILE = path.join(__dirname, 'libhlvm-runtime.a');
+
+export const RUNTIME_DEFINITION_FILE = path.join(__dirname, 'lib.runtime.d.ts');
