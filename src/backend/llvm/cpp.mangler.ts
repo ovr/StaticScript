d65fd53df812d3bdf21d4f3806b28770a6b1b1a2
Dmitry Patsura <talk@dmtry.me>
2018-11-18 17:01:07 +0300

    Feature(compiler): CPPMangler - support fn/method without parameters

--- src/backend/llvm/cpp.mangler.ts
+++ src/backend/llvm/cpp.mangler.ts
@@ -1,52 +1,41 @@
 
 import * as ts from 'typescript';
 
+function mangleParameters(parameters: ts.NodeArray<ts.ParameterDeclaration>): string {
+    if (parameters.length > 0) {
+        return parameters.map(
+            (parameter) => {
+                if (parameter.type) {
+                    switch (parameter.type.kind) {
+                        case ts.SyntaxKind.NumberKeyword:
+                            return 'd';
+                        case ts.SyntaxKind.StringKeyword:
+                            return 'PKc';
+                        case ts.SyntaxKind.BooleanKeyword:
+                            return 'b';
+                        default:
+                            throw new Error(
+                                `Unsupported mangling parameter type: ${parameter.type.kind}`
+                            );
+                    }
+                }
+
+                throw new Error('Unsupported mangling without type');
+            }
+        ).join();
+    }
+
+    return 'v';
+}
+
 export class CPPMangler {
     static getMethodName(clazz: string, method: string, parameters: ts.NodeArray<ts.ParameterDeclaration>): string {
         const name = clazz + '__' + method;
 
-        return '_Z' + name.length + name + parameters.map(
-            (parameter) => {
-                if (parameter.type) {
-                    switch (parameter.type.kind) {
-                        case ts.SyntaxKind.NumberKeyword:
-                            return 'd';
-                        case ts.SyntaxKind.StringKeyword:
-                            return 'PKc';
-                        case ts.SyntaxKind.BooleanKeyword:
-                            return 'b';
-                        default:
-                            throw new Error(
-                                `Unsupported mangling parameter type: ${parameter.type.kind}`
-                            );
-                    }
-                }
-
-                throw new Error('Unsupported mangling without type');
-            }
-        );
+        return '_Z' + name.length + name + mangleParameters(parameters);
     }
 
     static getFunctionName(name: string, parameters: ts.NodeArray<ts.ParameterDeclaration>): string {
-        return '_Z' + name.length + name + parameters.map(
-            (parameter) => {
-                if (parameter.type) {
-                    switch (parameter.type.kind) {
-                        case ts.SyntaxKind.NumberKeyword:
-                            return 'd';
-                        case ts.SyntaxKind.StringKeyword:
-                            return 'PKc';
-                        case ts.SyntaxKind.BooleanKeyword:
-                            return 'b';
-                        default:
-                            throw new Error(
-                                `Unsupported mangling parameter type: ${parameter.type.kind}`
-                            );
-                    }
-                }
-
-                throw new Error('Unsupported mangling without type');
-            }
-        );
+        return '_Z' + name.length + name + mangleParameters(parameters);
     }
 }
