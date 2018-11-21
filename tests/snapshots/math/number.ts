d989ceb3895859b1cee5b5760eb8bb08ede72906
Dmitry Patsura <talk@dmtry.me>
2018-11-22 01:28:30 +0300

    Feature(compiler): Add first test for compile

---
+++ tests/snapshots/math/number.ts
@@ -0,0 +1,55 @@
+
+{
+    function simpleAdd(): number {
+        const a: number = 5;
+        const b: number = 10;
+
+        return a + b;
+    }
+
+    function simpleSub(): number {
+        const a: number = 5;
+        const b: number = 10;
+
+        return a - b;
+    }
+
+    function simpleDiv(): number {
+        const a: number = 5;
+        const b: number = 10;
+
+        return a / b;
+    }
+
+    function simpleMul(): number {
+        const a: number = 5;
+        const b: number = 10;
+
+        return a * b;
+    }
+
+    // function mathFloor(): number {
+    //     return Math.floor(5.45);
+    // }
+    //
+    // function mathCeil(): number {
+    //     return Math.floor(5.45);
+    // }
+    //
+    // function mathSqrt(): number {
+    //     return Math.sqrt(4);
+    // }
+    //
+    // function mathPow(): number {
+    //     return Math.pow(4, 4);
+    // }
+
+    simpleAdd();
+    simpleSub();
+    simpleDiv();
+    simpleMul();
+    // mathFloor();
+    // mathCeil();
+    // mathSqrt();
+    // mathPow();
+}
