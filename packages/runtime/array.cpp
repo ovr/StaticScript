4feec1d77b87e3ecce22da170d0a690aa92e2210
Dmitry Patsura <talk@dmtry.me>
2018-12-21 21:49:30 +0900

    Feature(runtime): Add constructors for typed int8/uint8/int16/uint16 arrays

--- packages/runtime/array.cpp
+++ packages/runtime/array.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "array.h"
+
+LIBRARY_EXPORT Array<int8_t>* Int8ArrayConstructor__constructor(double size) {
+    return new Array<int8_t>(static_cast<int32_t>(size));
+}
+
+LIBRARY_EXPORT Array<uint8_t>* Uint8ArrayConstructor__constructor(double size) {
+    return new Array<uint8_t>(static_cast<int32_t>(size));
+}
+
+LIBRARY_EXPORT Array<int16_t>* Int16ArrayConstructor__constructor(double size) {
+    return new Array<int16_t>(static_cast<int32_t>(size));
+}
+
+LIBRARY_EXPORT Array<uint16_t>* Uint16ArrayConstructor__constructor(double size) {
+    return new Array<uint16_t>(static_cast<int32_t>(size));
+}
